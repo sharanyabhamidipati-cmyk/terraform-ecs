68abf3a4fa05a7b7cc51d88030665037f4005bbb
root <root@LAPTOP-R09UB6MI.localdomain>
2026-03-01 15:22:30 +0530

    updated files

--- ecs.tf
+++ ecs.tf
@@ -14,11 +14,35 @@ resource "aws_ecs_task_definition" "app" {
     {
       name  = var.app_name
       image = var.image_url
+
       portMappings = [
         {
           containerPort = 8080
+          hostPort      = 8080
         }
       ]
     }
   ])
+}
+
+resource "aws_ecs_service" "app" {
+  name            = var.app_name
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.app.arn
+  launch_type     = "FARGATE"
+  desired_count   = 1
+
+  network_configuration {
+    subnets         = var.public_subnets
+    security_groups = [aws_security_group.ecs_service.id]
+    assign_public_ip = true
+  }
+
+  load_balancer {
+    target_group_arn = aws_lb_target_group.app.arn
+    container_name   = var.app_name
+    container_port   = 8080
+  }
+
+  depends_on = [aws_lb_listener.app]
 }
